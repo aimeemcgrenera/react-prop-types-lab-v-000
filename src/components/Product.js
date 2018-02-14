f55a362742441f9d1ecd612eceed9d7e5f6d7de8
Aimee McGrenera <aimee.mcgrenera@gmail.com>
2018-02-14 17:39:55 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -22,11 +22,26 @@ Product.defaultProps = {
   hasWatermark: false,
 };
  
-Product.propTypes = {
+ Product.propTypes = {
   name: PropTypes.string.isRequired,
   producer: PropTypes.string,
   hasWatermark: PropTypes.bool,
-  color: PropTypes.string.isRequired,
+  color: PropTypes.oneOf([
+    'white', 'eggshell-white', 'salmon'
+  ]).isRequired,
+  weight: function(props, propName, componentName) {
+    let weight = props[propName];
+    
+    if (weight === undefined) {
+      return new Error("Sorry you must include a number for weight.");
+    }
+
+    if (isNaN(weight)) {
+      return new Error("Sorry weight must be a number.");
+    }
+
+    return weight >= 80 && weight <= 300 ? null : new Error("Must be within range of 80 to 300");
+  }
 };
 
 export default Product;
